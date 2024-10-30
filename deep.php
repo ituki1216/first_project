e4d44b2e00569906652c5049c7344e4522a9b2b4
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-30 15:40:16 +0900

    Update deep.php
    https://www.php.net/manual/ja/langref.php

--- deep.php
+++ deep.php
@@ -1,1 +1,27 @@
+<?php echo "hello: php勉強中です"; // ファイルがphpのみの場合 終了タブは書かなくてもよい
+<?= "hello" ?>
+
+<?php if ($expression == True): ?>
+  条件式が真の場合にこれは表示されます
+<?php else: ?>
+  それ以外の場合はここが表示されますね
+<?php endif; ?>
+
+<?php echo "AAAAA"; >?
+  AAAA
+
+<?php echo "null, bool, int, float, string, array, object, collable, resour"; ?>
+
+<?php echo 
+//　数値があれば、４をたす
+if (is_int($an_int)) { //is???は指定した？？？がなになにであればちいうこと
+  $an_int += 4;
+}
+
+if (is_string($a_bool)) {
+  echo "string: $a_bool";
+} ?>
+
+
+
 
