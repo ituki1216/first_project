18c0d5005e5a7bbce2fad8139ff5d8fe4c305917
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-10-31 13:56:07 +0900

    Update calendar-index.php

--- calendar-index.php
+++ calendar-index.php
@@ -3,26 +3,41 @@
 
 <head>
   <meta charset="utf-8">
-  <title>月送りカレンダー</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>月送りカレンダー</title>							
 </head>
 
 <body>
   <div class="container"> 
-    <h3>
+    <h2>
       <a href="?ym=<?php echo $prev; ?>">&lt;</a> 
       <?php echo $calendar; ?>
-      <a href="?ym=<?php echo $next; ?>">&lt;</a>
-    </h3>
-      <table class="calendar-table">
-        <tr>
-          <th>日</th>
-          <th>月</th>
-          <th>火</th>
-          <th>水</th>
-          <th>木</th>
-          <th>金</th>
-          <th>土</th>
-        </tr>
-      </table>    
+      <a href="?ym=<?php echo $next; ?>">&gt;</a> 
+    </h2>
+    <table class="calendar-table">
+      <tr>
+        <?php 
+          $days = ["日", "月", "火", "水", "木", "金", "土"];
+          foreach ($days as $day) {
+            echo "<th>$day</th>";
+          }
+        ?>
+      </tr>
+      <?php
+        for ($week = 0; $week < 5; $week++) {
+          echo "<tr>";
+          for ($day = 1; $day <= 7; $day++) {
+            $calendar = $week * 7 + $day;
+            if ($calendar <= 31) {
+              echo "<td>{$calendar}</td>";
+            } else {
+              echo "<td></td>";
+            }
+          }
+          echo "</tr>";
+        }
+      ?>
+    </table>
   </div>
 </body>
+</html>
